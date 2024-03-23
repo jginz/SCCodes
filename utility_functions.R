175c5103a5cbbcf9c100103e1620f17d6f9d0203
Joshua David Ginzel <139583940+jginz@users.noreply.github.com>
2024-03-23 16:54:31 -0400

    Update utility_functions.R

--- utility_functions.R
+++ utility_functions.R
@@ -482,6 +482,91 @@ RunSCType3 = function(seu,custom_gene_list=NULL,tissue_type="Immune system",plot
 }
 
 ###################################################################
+## Run SCType For Integrated Objects
+###################################################################
+## Main function
+
+#' @description GNU General Public License v3.0 
+#' (https://github.com/IanevskiAleksandr/sc-type/blob/master/LICENSE)
+#' @author Aleksandr Ianevski <aleksandr.ianevski@helsinki.fi>, June 2021
+
+##' @custom_gene_list is the path to the gene list for SCType
+##' @tissue_type is the unique character describing cell types
+##' @plot plotting function for cell types
+
+RunSCTypeInt = function(seu,custom_gene_list=NULL,tissue_type="Immune system",plot=T){
+  
+  source("https://raw.githubusercontent.com/IanevskiAleksandr/sc-type/master/R/sctype_score_.R")
+  source("https://raw.githubusercontent.com/IanevskiAleksandr/sc-type/master/R/gene_sets_prepare.R")
+  
+  if(is.null(custom_gene_list)){
+    db_ = "https://raw.githubusercontent.com/jginz/SCCodes/main/ScTypeDB_full.xlsx"
+    gs_list = gene_sets_prepare(db_,tissue_type)
+  }else{
+    db_ = custom_gene_list
+    gs_list = gene_sets_prepare(db_,tissue_type)
+  }
+  
+  set.seed(1)
+  es.max = sctype_score(as.matrix(GetAssayData(seu, slot = "integrated$data")), scaled = FALSE,
+                        gs = gs_list$gs_positive, gs2 = gs_list$gs_negative)
+  
+  cL_results = do.call("rbind", lapply(unique(seu@meta.data$Clusters), function(cl){
+    es.max.cl = sort(rowSums(es.max[ ,rownames(seu@meta.data[seu@meta.data$Clusters==cl, ])]), decreasing = !0)
+    head(data.frame(cluster = cl, type = names(es.max.cl), scores = es.max.cl, ncells = sum(seu@meta.data$Clusters==cl)), 10)
+  }))
+  sctype_scores = cL_results %>% group_by(cluster) %>% top_n(n = 1, wt = scores)  
+  sctype_scores$type[as.numeric(as.character(sctype_scores$scores)) < sctype_scores$ncells/4] = "Unknown"
+  seu@meta.data$SCType2 = ""
+  for(j in unique(sctype_scores$cluster)){
+    cl_type = sctype_scores[sctype_scores$cluster==j,]; 
+    seu@meta.data$SCType2[seu@meta.data$Clusters == j] = as.character(cl_type$type[1])
+  }
+  if(plot){
+    # prepare edges
+    cL_results=cL_results[order(cL_results$cluster),]; 
+    edges = cL_results; edges$type = paste0(edges$type,"_",edges$cluster); 
+    edges$cluster = paste0("cluster ", edges$cluster); 
+    edges = edges[,c("cluster", "type")]; 
+    colnames(edges) = c("from", "to"); 
+    rownames(edges) <- NULL
+    
+    # prepare nodes
+    nodes_lvl1 = sctype_scores[,c("cluster", "ncells")]; 
+    nodes_lvl1$cluster = paste0("cluster ", nodes_lvl1$cluster); 
+    nodes_lvl1$Colour = "#f1f1ef"; nodes_lvl1$ord = 1; 
+    nodes_lvl1$realname = nodes_lvl1$cluster; 
+    nodes_lvl1 = as.data.frame(nodes_lvl1); nodes_lvl2 = c(); 
+    ccolss = dittoColors()
+    for (i in 1:length(unique(cL_results$cluster))){
+      dt_tmp = cL_results[cL_results$cluster == unique(cL_results$cluster)[i], ]; 
+      nodes_lvl2 = rbind(nodes_lvl2, data.frame(cluster = paste0(dt_tmp$type,"_",dt_tmp$cluster), 
+                                                ncells = dt_tmp$scores, Colour = ccolss[i], ord = 2, realname = dt_tmp$type))
+    }
+    nodes = rbind(nodes_lvl1, nodes_lvl2); nodes$ncells[nodes$ncells<1] = 1;
+    files_db = openxlsx::read.xlsx(db_)[,c("cellName","shortName")];
+    files_db = unique(files_db); 
+    nodes = merge(nodes, files_db, all.x = T, all.y = F, by.x = "realname", by.y = "cellName", sort = F)
+    nodes$shortName[is.na(nodes$shortName)] = nodes$realname[is.na(nodes$shortName)]; 
+    nodes = nodes[,c("cluster", "ncells", "Colour", "ord", "shortName", "realname")]
+    
+    mygraph <- graph_from_data_frame(edges, vertices=nodes)
+    
+    # Make the graph
+    gggr<- ggraph(mygraph, layout = 'circlepack', weight=I(ncells)) + 
+      geom_node_circle(aes(filter=ord==1,fill=I("#F5F5F5"), colour=I("#D3D3D3")), alpha=0.9) + 
+      geom_node_circle(aes(filter=ord==2,fill=I(Colour), colour=I("#D3D3D3")), alpha=0.9) +
+      theme_void() + geom_node_text(aes(filter=ord==2, label=shortName, 
+                                        colour=I("#ffffff"), fill="white", repel = !1, parse = T, size = I(log(ncells,25)*1.5))) + 
+      geom_node_label(aes(filter=ord==1,label=shortName, colour=I("#000000"), size = I(3), fill="white", parse = T), 
+                      repel = !0, segment.linetype="dotted")
+    gridExtra::grid.arrange(DimPlot(seu, reduction = "umap", label = TRUE, repel = TRUE, cols = ccolss), gggr)
+    
+  }
+  return(seu)
+}
+
+###################################################################
 ## Run Single Sample GSEA using UCell
 ## Source: 
 ## https://github.com/carmonalab/UCell
